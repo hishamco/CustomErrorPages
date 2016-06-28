1b171fab390b2072bdbdb70262c6a80e7a0288b0
hishamco <hishamco_2007@yahoo.com>
2016-06-28 06:23:51 +0300

    Remove unecessary using

--- CustomErrorPagesMiddleware.cs
+++ CustomErrorPagesMiddleware.cs
@@ -1,10 +1,8 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace CustomErrorPages
